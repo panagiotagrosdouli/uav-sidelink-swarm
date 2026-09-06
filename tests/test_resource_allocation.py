89c66f339b7284313a7b4c32fd44ee899eff15fa
panagiotagrosdouli <75089541+panagiotagrosdouli@users.noreply.github.com>
2026-09-06 10:51:30 +0300

    Test graph-conflict resource allocation

--- tests/test_resource_allocation.py
+++ tests/test_resource_allocation.py
@@ -1,6 +1,10 @@
 import numpy as np
 
-from src.sidelink.resource_allocation import greedy_distance_aware_allocation, random_allocation
+from src.sidelink.resource_allocation import (
+    graph_conflict_allocation,
+    greedy_distance_aware_allocation,
+    random_allocation,
+)
 
 
 def test_random_allocation_is_reproducible():
@@ -10,8 +14,18 @@ def test_random_allocation_is_reproducible():
 
 
 def test_greedy_allocation_returns_valid_resources():
-    tx = np.array([[0,0,0],[10,0,0],[20,0,0]], dtype=float)
-    rx = np.array([[1,0,0],[11,0,0],[21,0,0]], dtype=float)
+    tx = np.array([[0, 0, 0], [10, 0, 0], [20, 0, 0]], dtype=float)
+    rx = np.array([[1, 0, 0], [11, 0, 0], [21, 0, 0]], dtype=float)
     out = greedy_distance_aware_allocation(tx, rx, 2).resources
     assert len(out) == 3
     assert np.all((out >= 0) & (out < 2))
+
+
+def test_graph_conflict_allocation_is_valid_and_deterministic():
+    tx = np.array([[0, 0, 0], [10, 0, 0], [20, 0, 0], [30, 0, 0]], dtype=float)
+    rx = np.array([[1, 0, 0], [11, 0, 0], [21, 0, 0], [31, 0, 0]], dtype=float)
+    a = graph_conflict_allocation(tx, rx, 2)
+    b = graph_conflict_allocation(tx, rx, 2)
+    assert a.algorithm == "graph_conflict_weighted"
+    assert np.array_equal(a.resources, b.resources)
+    assert np.all((a.resources >= 0) & (a.resources < 2))
