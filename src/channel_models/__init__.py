cf7ce51a70a6b2ee1f5f335e6c8e6295ec450380
panagiotagrosdouli <75089541+panagiotagrosdouli@users.noreply.github.com>
2026-09-06 09:44:48 +0300

    Add channel models package

---
+++ src/channel_models/__init__.py
@@ -0,0 +1,5 @@
+"""Propagation-channel models used by the UAV sidelink simulator."""
+
+from .measured_a2a import path_loss_db as measured_a2a_path_loss_db
+
+__all__ = ["measured_a2a_path_loss_db"]
